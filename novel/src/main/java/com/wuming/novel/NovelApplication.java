92493e74a885cd1bc694170d4cca1c6f30d3570f
wuming <gpn121@126.com>
2026-06-15 17:29:27 +0800

    增加@EnableAsync注解

--- novel/src/main/java/com/wuming/novel/NovelApplication.java
+++ novel/src/main/java/com/wuming/novel/NovelApplication.java
@@ -3,7 +3,9 @@ package com.wuming.novel;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @MapperScan(basePackages = "com.wuming.novel.mapper")
 public class NovelApplication {
