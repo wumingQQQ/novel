4e79566e54639669101856eaf71bb03adcaf65e9
wuming <gpn121@126.com>
2026-06-15 13:57:39 +0800

    为mapper增加包扫描

--- novel/src/main/java/com/wuming/novel/NovelApplication.java
+++ novel/src/main/java/com/wuming/novel/NovelApplication.java
@@ -1,9 +1,11 @@
 package com.wuming.novel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.wuming.novel.mapper")
 public class NovelApplication {
 
     public static void main(String[] args) {
