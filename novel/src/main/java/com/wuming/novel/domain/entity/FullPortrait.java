9e2ff2574b5aeb45e095ded8882853758a65668d
wuming <gpn121@126.com>
2026-06-18 14:42:56 +0800

    完整角色画像

---
+++ novel/src/main/java/com/wuming/novel/domain/entity/FullPortrait.java
@@ -0,0 +1,9 @@
+package com.wuming.novel.domain.entity;
+
+import lombok.Data;
+
+@Data
+public class FullPortrait {
+    CharacterProfile characterProfile;
+    InteractionProfile interactionProfile;
+}
