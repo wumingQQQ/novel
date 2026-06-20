254210654472487c24c8595193ecdb6e47745f30
wuming <gpn121@126.com>
2026-06-20 20:04:04 +0800

    移除多余的类

--- novel/src/main/java/com/wuming/novel/domain/entity/FullPortrait.java
+++
@@ -1,9 +0,0 @@
-package com.wuming.novel.domain.entity;
-
-import lombok.Data;
-
-@Data
-public class FullPortrait {
-    CharacterProfile characterProfile = new CharacterProfile();
-    InteractionProfile interactionProfile = new InteractionProfile();
-}
