3f8fd5508d7b1317d99042c47b7b238de11b77de
wuming <gpn121@126.com>
2026-06-23 21:14:49 +0800

    llm返回内容错误的异常

---
+++ novel/src/main/java/com/wuming/novel/llm/checker/LlmResponseCheckException.java
@@ -0,0 +1,7 @@
+package com.wuming.novel.llm.checker;
+
+public class LlmResponseCheckException extends RuntimeException {
+    public LlmResponseCheckException(String message) {
+        super(message);
+    }
+}
