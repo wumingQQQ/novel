236865d0b955bd889b4bd68cf524102832ec91f8
wuming <gpn121@126.com>
2026-06-28 17:28:57 +0800

    配置不同事件在相同topic下的不同tag

---
+++ novel/src/main/java/com/wuming/novel/message/rocketmq/RocketMqTagProperties.java
@@ -0,0 +1,13 @@
+package com.wuming.novel.message.rocketmq;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "novel.mq.tags")
+public class RocketMqTagProperties {
+    private String singleChapterSplitCompleted;
+    private String jobFinished;
+}
