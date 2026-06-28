3a5a22a39a79bad2378ecdd0cee3d6cbbfec626e
wuming <gpn121@126.com>
2026-06-28 17:30:00 +0800

    配置仅测试事件发布者仅在novel.mq.enabled=false时生效

--- novel/src/main/java/com/wuming/novel/message/jobdone/JobFinishEventPublisher.java
+++ novel/src/main/java/com/wuming/novel/message/jobdone/JobFinishEventPublisher.java
@@ -2,10 +2,12 @@ package com.wuming.novel.message.jobdone;
 
 import com.wuming.novel.message.EventPublisher;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(name = "novel.mq.enabled", havingValue = "false", matchIfMissing = true)
 public class JobFinishEventPublisher implements EventPublisher<JobFinishEvent> {
     @Override
     public void publish(JobFinishEvent event) {
--- novel/src/main/java/com/wuming/novel/message/scenesplit/ChapterSceneSplitCompleteEventPublisher.java
+++ novel/src/main/java/com/wuming/novel/message/scenesplit/ChapterSceneSplitCompleteEventPublisher.java
@@ -2,11 +2,13 @@ package com.wuming.novel.message.scenesplit;
 
 import com.wuming.novel.message.EventPublisher;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 
 @Slf4j
 @Component
+@ConditionalOnProperty(name = "novel.mq.enabled", havingValue = "false", matchIfMissing = true)
 public class ChapterSceneSplitCompleteEventPublisher implements EventPublisher<ChapterSceneSplitCompleteEvent> {
     @Override
     public void publish(ChapterSceneSplitCompleteEvent event) {
