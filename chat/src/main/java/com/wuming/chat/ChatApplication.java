314f7cd1e3951178ecf26ca5e766f7323c76967f
wuming <gpn121@126.com>
2026-06-28 21:19:02 +0800

    回到starter-openai依赖

--- chat/src/main/java/com/wuming/chat/ChatApplication.java
+++ chat/src/main/java/com/wuming/chat/ChatApplication.java
@@ -2,25 +2,12 @@ package com.wuming.chat;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioTranscriptionAutoConfiguration;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiChatAutoConfiguration;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiEmbeddingAutoConfiguration;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiImageAutoConfiguration;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiModerationAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableDubbo
 @MapperScan("com.wuming.chat.mapper")
-@SpringBootApplication(exclude = {
-        OpenAiAudioSpeechAutoConfiguration.class,
-        OpenAiAudioTranscriptionAutoConfiguration.class,
-        OpenAiChatAutoConfiguration.class,
-        OpenAiEmbeddingAutoConfiguration.class,
-        OpenAiImageAutoConfiguration.class,
-        OpenAiModerationAutoConfiguration.class
-})
+@SpringBootApplication
 public class ChatApplication {
 
     public static void main(String[] args) {
