3c0871315715469bd4eb632fa7dd6b3df2e91b28
wuming <gpn121@126.com>
2026-07-01 11:24:50 +0800

    统一各个模块中的全局异常处理器继承BaseExceptionHandler

--- novel/src/main/java/com/wuming/novel/exception/GlobalExceptionHandler.java
+++ novel/src/main/java/com/wuming/novel/exception/GlobalExceptionHandler.java
@@ -1,26 +1,8 @@
 package com.wuming.novel.exception;
 
-import com.wuming.common.web.ApiResponse;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.wuming.common.exception.BaseExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(FileTooLargeException.class)
-    public ApiResponse<Void> handleException(FileTooLargeException ex) {
-        return ApiResponse.error(413, ex.getMessage());
-    }
-
-    @ExceptionHandler(FileNotSupportException.class)
-    public ApiResponse<Void> handleException(FileNotSupportException ex) {
-        return ApiResponse.error(400, ex.getMessage());
-    }
-
-
-    @ExceptionHandler(Exception.class)
-    public ApiResponse<Void> handleException(Exception ex) {
-        return ApiResponse.error(500, ex.getMessage());
-    }
-
+public class GlobalExceptionHandler extends BaseExceptionHandler {
 }
