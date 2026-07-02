cd410c69f24a4f64f5cc70f4b5e95ea9b63c2902
wuming <gpn121@126.com>
2026-07-02 16:13:00 +0800

    refactor: 将自定义插件的自动配置在Mybatisplus配置之后

--- common/src/main/java/com/wuming/common/mybatis/MybatisMonitorAutoConfiguration.java
+++ common/src/main/java/com/wuming/common/mybatis/MybatisMonitorAutoConfiguration.java
@@ -2,11 +2,13 @@ package com.wuming.common.mybatis;
 
 import org.apache.ibatis.plugin.Interceptor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
+@AutoConfigureBefore(name = "com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration")
 @ConditionalOnClass(Interceptor.class)
 public class MybatisMonitorAutoConfiguration {
 
