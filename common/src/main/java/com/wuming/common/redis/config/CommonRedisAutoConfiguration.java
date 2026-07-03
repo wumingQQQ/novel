4a2cdc7f0598d81e23a6781bc9ee3ce5aaae867f
wuming <gpn121@126.com>
2026-07-03 10:05:29 +0800

    refactor: 避免传递依赖不需要的包

--- common/src/main/java/com/wuming/common/redis/config/CommonRedisAutoConfiguration.java
+++ common/src/main/java/com/wuming/common/redis/config/CommonRedisAutoConfiguration.java
@@ -5,15 +5,16 @@ import com.wuming.common.redis.core.RedisHashOps;
 import com.wuming.common.redis.core.RedisJsonOps;
 import com.wuming.common.redis.core.RedisListOps;
 import com.wuming.common.redis.core.RedisStringOps;
-import com.wuming.common.redis.vector.RedisVectorStoreFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @AutoConfiguration(after = RedisAutoConfiguration.class)
+@ConditionalOnClass(StringRedisTemplate.class)
 @RequiredArgsConstructor
 public class CommonRedisAutoConfiguration {
 
@@ -49,11 +50,5 @@ public class CommonRedisAutoConfiguration {
             StringRedisTemplate redisTemplate
     ) {
         return new RedisListOps(redisTemplate);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public RedisVectorStoreFactory redisVectorStoreFactory() {
-        return new RedisVectorStoreFactory();
     }
 }
