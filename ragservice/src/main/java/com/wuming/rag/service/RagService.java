678c06666e237d7e9e9ac28e3da5c7e44dca117e
wuming <gpn121@126.com>
2026-07-05 14:01:16 +0800

    feat(rag): 完成批量插入与删除文档的功能

--- ragservice/src/main/java/com/wuming/rag/service/RagService.java
+++ ragservice/src/main/java/com/wuming/rag/service/RagService.java
@@ -2,22 +2,22 @@ package com.wuming.rag.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.wuming.api.rag.RagFacade;
-import com.wuming.api.rag.dto.CreateIndexRequest;
-import com.wuming.api.rag.dto.DeleteDocumentRequest;
-import com.wuming.api.rag.dto.SearchHit;
-import com.wuming.api.rag.dto.UpsertDocumentRequest;
+import com.wuming.api.rag.dto.*;
 import com.wuming.api.rag.dto.spec.PassageSearchRequest;
 import com.wuming.api.rag.dto.spec.ReactionRuleSearchRequest;
 import com.wuming.api.rag.dto.spec.RoleExampleSearchRequest;
 import com.wuming.rag.model.RagIndexDefinition;
 import com.wuming.rag.rerank.RerankService;
+import com.wuming.rag.util.RedisByteUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.ai.embedding.EmbeddingModel;
 import redis.clients.jedis.JedisPooled;
 import redis.clients.jedis.search.FTCreateParams;
 import redis.clients.jedis.search.IndexDataType;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 
@@ -28,6 +28,7 @@ public class RagService implements RagFacade {
     private final RerankService rerankService;
     private final RedisIndexDefinitionStore definitionStore;
     private final JedisPooled jedisPool;
+    private final EmbeddingModel embeddingModel;
 
     @Override
     public boolean createIndex(CreateIndexRequest request) {
@@ -50,19 +51,62 @@ public class RagService implements RagFacade {
             return true;
         }
         catch (JsonProcessingException e) {
-            log.error("索引序列化失败", e);
+            log.error("索引定义序列化失败", e);
             return false;
         }
     }
 
     @Override
     public int upsertDocuments(UpsertDocumentRequest request) {
-        return 0;
+        int success = 0;
+        try {
+            log.info("开始插入文档");
+            RagIndexDefinition definition = definitionStore.getRequired(request.getIndexName());
+            if(request.getDocuments() == null || request.getDocuments().isEmpty()){
+                return 0;
+            }
+            List<String> texts = request.getDocuments().stream()
+                    .map(RagDocument::getText).toList();
+
+            List<float[]> embeddings = embeddingModel.embed(texts);
+            for(int i = 0; i < request.getDocuments().size(); i++){
+                RagDocument doc = request.getDocuments().get(i);
+                String redisKey = definition.getKeyPrefix() + doc.getDocumentId();
+
+                // 由于需要保存数组，所以参数需为byte[]
+                jedisPool.hset(
+                    redisKey.getBytes(StandardCharsets.UTF_8),
+                    RedisByteUtil.hashFields(doc, embeddings.get(i))
+                );
+                success++;
+            }
+        }
+        catch (JsonProcessingException e) {
+            log.error("反序列化索引定义失败", e);
+            log.error("文档插入失败");
+        }
+        return success;
     }
 
     @Override
     public int deleteDocuments(DeleteDocumentRequest request) {
-        return 0;
+        try {
+            log.info("开始删除文档");
+            RagIndexDefinition definition = definitionStore.getRequired(request.getIndexName());
+            if(request.getDocumentIds() == null || request.getDocumentIds().isEmpty()){
+                return 0;
+            }
+
+            List<String> redisKeys = request.getDocumentIds().stream()
+                    .map(docId -> definition.getKeyPrefix() + docId)
+                    .toList();
+            return (int) jedisPool.del(redisKeys.toArray(new String[0]));
+        }
+        catch (JsonProcessingException e) {
+            log.error("反序列化索引定义时失败", e);
+            log.error("删除操作失败");
+            return 0;
+        }
     }
 
     @Override
