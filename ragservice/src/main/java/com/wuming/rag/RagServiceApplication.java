6707c4ffcc5bf628087f380d2df06e558098224d
wuming <gpn121@126.com>
2026-07-07 20:43:51 +0800

    refactor(rag): 移除openai的自动装配

--- ragservice/src/main/java/com/wuming/rag/RagServiceApplication.java
+++ ragservice/src/main/java/com/wuming/rag/RagServiceApplication.java
@@ -1,11 +1,24 @@
 package com.wuming.rag;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioTranscriptionAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiChatAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiEmbeddingAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiImageAutoConfiguration;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiModerationAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableDubbo
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        OpenAiAudioSpeechAutoConfiguration.class,
+        OpenAiAudioTranscriptionAutoConfiguration.class,
+        OpenAiChatAutoConfiguration.class,
+        OpenAiEmbeddingAutoConfiguration.class,
+        OpenAiImageAutoConfiguration.class,
+        OpenAiModerationAutoConfiguration.class
+})
 public class RagServiceApplication {
 
     public static void main(String[] args) {
