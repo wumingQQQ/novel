0d876fe47996e190482febb10dad54c14b08b403
wuming <gpn121@126.com>
2026-07-13 10:26:50 +0800

    fix: 注册任务分页配置

--- novel/src/main/java/com/wuming/novel/NovelApplication.java
+++ novel/src/main/java/com/wuming/novel/NovelApplication.java
@@ -1,9 +1,11 @@
 package com.wuming.novel;
 
+import com.wuming.novel.config.TaskPageProperties;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 @MapperScan(basePackages = "com.wuming.novel.infrastructure.mapper")
+@EnableConfigurationProperties(TaskPageProperties.class)
 public class NovelApplication {
 
     public static void main(String[] args) {
